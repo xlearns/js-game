d2702332107561f496aa30e4924f4a80e4757677
xlearns <2447684095@qq.com>
2022-05-01 15:14:55 -0800

    '帧动画切换'

---
+++ config.js
@@ -0,0 +1,14 @@
+const action = {
+  static: [177, 180],
+  idle: [91, 96],
+  walk: [181, 188],
+  run: [106, 113],
+  die: [100, 104],
+  attack1: [1, 10],
+  attack2: [11, 21],
+  attack3: [34, 42],
+  frontThornAttack: [114, 123],
+  specked: [134, 139],
+  pickUp: [34, 42],
+  ghost: [140, 150],
+};
